6da216ada16961a5481d78f978440d419216885b
Skywave22 <lovepet411+1@gmail.com>
2026-07-12 07:05:34 +0500

    Delete upload-1 directory

--- upload-1/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file for BluePilot Remote v3.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-}
